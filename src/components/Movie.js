be99741c0892d9fc898f2943f05b10263f25fec5
mifta <smiftakhus@gmail.com>
2022-02-17 21:12:45 +0700

    <ref>(common): fix bug

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import Skeleton from '@mui/material/Skeleton';
@@ -9,7 +9,7 @@ const MovieBox = styled('div')({
   justifyContent: 'center',
   gridGap: '30px',
   marginTop: '60px',
-  marginBottom: '100px',
+  paddingBottom: '100px',
 });
 const Item = styled('div')({
   height: 'max-content',
@@ -33,47 +33,45 @@ const TitleWrap = styled('div')({
   placeItems: 'center',
 });
 
-const Movie = ({ movies, search }) => {
+const Movie = (props) => {
+  const [movies, setMovies] = useState(props.movies);
+  const [loading, setLoading] = useState(props.loading);
+
+  useEffect(() => {
+    setMovies(props.movies);
+    setLoading(props.loading);
+  }, [props]);
+
   return (
     <Container>
       <MovieBox>
-        {search ? (
-          search.map((item, i) => (
-            <Item key={item.Title + i}>
-              <ImageWrap>
-                <Image src={item.Poster} alt={item.Title} />
-              </ImageWrap>
-              <TitleWrap>
-                <Typography
-                  variant="h6"
-                  sx={{ color: 'white', textAlign: 'center' }}
-                >
-                  {item.Title}
-                </Typography>
-              </TitleWrap>
-            </Item>
-          ))
-        ) : movies ? (
-          movies.map((item, i) => (
-            <Item key={item.Title}>
-              <ImageWrap>
-                <Image src={item.Poster} alt={item.Title} />
-              </ImageWrap>
-              <TitleWrap>
-                <Typography variant="p" sx={{ color: 'white' }}>
-                  {item.Title}
-                </Typography>
-              </TitleWrap>
-            </Item>
-          ))
-        ) : (
-          <>
-            <Skeleton variant="rectangular" width={250} height={370} />
-            <Skeleton variant="rectangular" width={250} height={370} />
-            <Skeleton variant="rectangular" width={250} height={370} />
-            <Skeleton variant="rectangular" width={250} height={370} />
-          </>
-        )}
+        {
+          loading ?
+            (
+              <>
+                <Skeleton variant="rectangular" width={250} height={370} />
+                <Skeleton variant="rectangular" width={250} height={370} />
+                <Skeleton variant="rectangular" width={250} height={370} />
+                <Skeleton variant="rectangular" width={250} height={370} />
+              </>
+            ) :
+            movies.length !== 0 ?
+              movies.map((item, i) => (
+                <Item key={item.imdbID} className='hover:cursor-pointer hover:shadow-xl'>
+                  <ImageWrap>
+                    <Image src={item.Poster} alt={item.Title} />
+                  </ImageWrap>
+                  <TitleWrap>
+                    <Typography variant="p" sx={{ color: 'white' }}>
+                      {item.Title}
+                    </Typography>
+                  </TitleWrap>
+                </Item>
+              ))
+              : (
+                <Typography>Data Tidak Ditemukan</Typography>
+              )
+        }
       </MovieBox>
     </Container>
   );
